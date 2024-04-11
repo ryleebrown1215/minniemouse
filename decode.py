66e0dc5b7682b6f3d31d35cac6423c099ccd6ac2
Rylee Brown <ryleebrown@ufl.edu>
2024-04-11 11:22:14 -0400

    This is the decoder

--- decode.py
+++ decode.py
@@ -1,7 +1,11 @@
 class Decode:
+
+    def __init__(self, encoded_string):
+        self.encoded_string = encoded_string
+
+
     def decode(self, encoded_string):
         original_string = ''
-
 
         for i in encoded_string:
             i -= 3
